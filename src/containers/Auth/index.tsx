a21c28a372ef2447ef7a60550e636338c20d50aa
JD Wolk <jdwolk@gmail.com>
2024-05-14 20:38:08 -0700

    Shows errors on Auth page

--- src/containers/Auth/index.tsx
+++ src/containers/Auth/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import TextField from '../../components/TextField';
@@ -10,26 +11,50 @@ import styles from './styles.module.css';
 interface IFormValues {
   email: string;
   password: string;
+  form?: string;
 }
 
 function Auth() {
   const {
     register,
     handleSubmit,
+    setError,
+    watch,
     formState: { errors },
   } = useForm<IFormValues>()
+
+  const fields = watch();
+  const [fieldsAtErrorTime, setFieldsAtErrorTime] = useState<string>(undefined);
+
+  // Allows user to clear errors
+  // by typing in any field
+  useEffect(() => {
+    if (fieldsAtErrorTime) {
+      const currentFields = JSON.stringify(fields);
+      if (currentFields !== fieldsAtErrorTime) {
+        setFieldsAtErrorTime(undefined);
+        setError('form', undefined);
+      }
+    }
+  }, [fields, fieldsAtErrorTime]);
 
   const onSubmit = handleSubmit(async (data) => {
     const result = await Actions.authenticate(data)
     handle(result,
-      async (jwt) => {
+      async (_jwt) => {
         const result = await Actions.example();
         handle(result,
           ({ json }) => console.log('### Result: ', json),
           (failure) => console.log('### Failure: ', failure)
         )
       },
-      async (failure) => console.log('### Failure: ', failure),
+      async (_failure) => (
+        setFieldsAtErrorTime(JSON.stringify(fields)),
+        setError('form', {
+            type: 'manual',
+            message: 'Invalid email or password, please try again',
+        })
+      ),
     );
   });
 
@@ -40,6 +65,7 @@ function Auth() {
           <img className={styles.logo} src={PunchClockImage} />
           <h1>PUNCHCLOQ</h1>
           <h2>Sign In</h2>
+          {errors.form && <p className={styles.errorText}>{errors.form.message}</p>}
 
           <form onSubmit={onSubmit}>
             <TextField
